90180f0a2111256081c13021cf99e2f911c9b44a
WasteofSpaceYT <82965910+WasteofSpaceYT@users.noreply.github.com>
2022-02-18 13:13:27 -0500

    more updates

--- trader-jeff/src/Components/charts.tsx
+++ trader-jeff/src/Components/charts.tsx
@@ -1,18 +1,59 @@
 import { initializeApp } from "firebase/app";
 import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { Http2ServerRequest } from "http2";
 import React from "react";
 import { useCookies } from "react-cookie";
 import ReactDOM from "react-dom";
-var yahooFinance = require('yahoo-finance');
+import http from "https"
 
 function Chart(){
-	
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCoik6dAfUPkjTD4ediiG7phRzH3VKhthk",
+    authDomain: "paper-plate-5ab88.firebaseapp.com",
+    projectId: "paper-plate-5ab88",
+    storageBucket: "paper-plate-5ab88.appspot.com",
+    messagingSenderId: "514869257382",
+    appId: "1:514869257382:web:374d1bd4bbeff81aa14067",
+    measurementId: "G-HJRSQKL6KM"
+  };
+  
+  // Initialize Firebase
+  const firebaseapp = initializeApp(firebaseConfig);
+  const app = getFirestore();
+
+  var useKey = 0
 	//@ts-ignore
-	const drawchart = (event) => {
+	const drawchart = async (event) => {
 		event.preventDefault();
 		var token = event.target[0].value
+    const options = {
+      "method": "GET",
+      "hostname": "stock-data-yahoo-finance-alternative.p.rapidapi.com",
+      "port": null,
+      "path": `/v8/finance/chart/${token}?comparisons=AMZN&events=div%2Csplit`,
+      "headers": {
+        "x-rapidapi-key": "fdce3efc23mshe63dce1b90f5763p1672e8jsn9c315c3a4421",
+        "x-rapidapi-host": "stock-data-yahoo-finance-alternative.p.rapidapi.com",
+        "useQueryString": true
+      }
+    };
+    //@ts-ignore
+    const req = http.request(options, function (res) {
+      const chunks: any[] = [];
     
-	}
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+    
+      res.on("end", function () {
+        const body = Buffer.concat(chunks);
+        console.log(body.toString());
+      });
+    });
+    
+    req.end();
+  }
 
     return(
 <>
